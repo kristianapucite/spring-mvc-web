3e2fd0a4ccdc8fcec83debd3273cb3af87f96ac3
Gusts Linkevics <gusts@gst.lv>
2020-12-14 21:37:27 +0200

    Add bootstrap css

--- src/main/java/com/company/springmvcweb/OwnerController.java
+++ src/main/java/com/company/springmvcweb/OwnerController.java
@@ -2,9 +2,13 @@ package com.company.springmvcweb;
 
 
 import com.company.springmvcweb.data.CarsRepository;
+import com.company.springmvcweb.dto.CarSearchDto;
+import com.company.springmvcweb.dto.OwnerSearchDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OwnerController {
@@ -20,11 +24,34 @@ public class OwnerController {
         return "owners";
     }
 
+//    @PostMapping("/owners")
+//    public String searchOwners(@ModelAttribute("searchDto") OwnerSearchDto searchDto, Model model) {
+//
+//        var repo = new CarsRepository();
+//        var items = repo.getOwners();
+//
+//        model.addAttribute("title", "Owners");
+//        model.addAttribute("owners", items);
+//
+//        return "owners";
+//    }
+
     @GetMapping("/cars")
     public String getCars(Model model) {
 
         var repo = new CarsRepository();
         var items = repo.getCars();
+
+        model.addAttribute("title", "Cars");
+        model.addAttribute("cars", items);
+
+        return "cars";
+    }
+
+    @PostMapping("/cars")
+    public String searchCars(@ModelAttribute("searchDto") CarSearchDto dto, Model model) {
+        var repo = new CarsRepository();
+        var items = repo.getCars(dto);
 
         model.addAttribute("title", "Cars");
         model.addAttribute("cars", items);
--- src/main/java/com/company/springmvcweb/data/CarsRepository.java
+++ src/main/java/com/company/springmvcweb/data/CarsRepository.java
@@ -1,5 +1,6 @@
 package com.company.springmvcweb.data;
 
+import com.company.springmvcweb.dto.CarSearchDto;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -41,6 +42,50 @@ public class CarsRepository {
 
         try {
             return session.createQuery("FROM Car").list();
+        } catch (HibernateException exception) {
+            System.err.println(exception);
+        } finally {
+            session.close();
+        }
+
+        return new ArrayList<>();
+    }
+
+    public Iterable<Car> getCars(CarSearchDto searchDto) {
+        var session = factory.openSession();
+
+        try {
+            var sql = "FROM Car";
+
+            if(!searchDto.getNumber().isBlank() || !searchDto.getVinNumber().isBlank()) {
+                sql += " where ";
+            }
+
+            if(!searchDto.getNumber().isBlank()) {
+                sql += " number = :search_number";
+            }
+
+            if(!searchDto.getVinNumber().isBlank()) {
+
+                if(!searchDto.getNumber().isBlank()) {
+                    sql += " and ";
+                }
+
+                sql += " vin_number = :search_vin_number";
+            }
+
+            var query = session.createQuery(sql);
+
+            if(!searchDto.getNumber().isBlank()) {
+                query.setParameter("search_number", searchDto.getNumber());
+            }
+
+            if(!searchDto.getVinNumber().isBlank()) {
+                query.setParameter("search_vin_number", searchDto.getVinNumber());
+            }
+
+            return query.list();
+
         } catch (HibernateException exception) {
             System.err.println(exception);
         } finally {
